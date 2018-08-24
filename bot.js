ae0a30c47eccbb40656adbb6105c1bd9e09d175c
gtherdgerthyhrtgefwthyujtyhfgds <42640657+gtherdgerthyhrtgefwthyujtyhfgds@users.noreply.github.com>
2018-08-24 12:27:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();   
+var prefix = "!";
 
 client.on('message', message => {
               if(!message.channel.guild) return;
@@ -7,7 +8,7 @@ client.on('message', message => {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "Cozmo bot";
+    let copy = "NO BC";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
