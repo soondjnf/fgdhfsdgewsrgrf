d28fb97edf0f32d380043a06fd0af332854f97d7
gtherdgerthyhrtgefwthyujtyhfgds <42640657+gtherdgerthyhrtgefwthyujtyhfgds@users.noreply.github.com>
2018-09-01 00:10:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,6 @@
 
 const Discord = require('discord.js');
-
 const client = new Discord.Client();   
-const translate = require('google-translate-api');   
-const fs = require("fs"); 
-const Canvas = require("canvas");
-const moment = require("moment");  
-
-const UserBlocked = new Set(); 
-const jimp = require('jimp');   
- const dateFormat = require('dateformat'); 
- const pretty = require('pretty-ms') 
 
 client.on('ready', () => {
 
@@ -18,96 +8,19 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
     console.log(`----------------`);
 });
+      const devs = ['463857132060540958','421023400971075585'];
+const adminprefix = "$"
+      
+client.on('message', message => {
+      if (!devs.includes(message.author.id)) return;
+  if (message.content.startsWith(adminprefix + 'slfstr')) {
+    if (!devs.includes(message.author.id)) return; 
+let args = message.content.split(' ').slice(1).join(' ');
 
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on('guildMemberAdd', member => {
-const channel = member.guild.channels.find("name","welcome")
-if (member.user.bot) return;
-var jimp = require('jimp')
-  var currentTime = new Date(),
-hours = currentTime.getHours() + 3 ,
-minutes = currentTime.getMinutes(),
-seconds = currentTime.getSeconds();
-if (minutes < 10) {
-minutes = "0" + minutes;
-}
-var suffix = "AM";
-if (hours >= 12) {
-suffix = "PM";
-hours = hours - 12;
-}
-if (hours == 0) {
-hours = 12;
-}
-const w = ['./20180806_230413.png'];
-              let Image = Canvas.Image,
-                  canvas = new Canvas(557, 241),
-                  ctx = canvas.getContext('2d');
-              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-                  if (err) return console.log(err)
-                  let BG = Canvas.Image;
-                  let ground = new Image;
-                  ground.src = Background;
-                  ctx.drawImage(ground, 0, 0, 557, 241);
-
-      })
-                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
-                      jimp.read(url, (err, ava) => {
-                          if (err) return console.log(err);
-                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                              if (err) return console.log(err);
-                                    ctx.font = '30px Arial Bold';
-                              ctx.fontSize = '20px';
-                              ctx.fillStyle = "#FFFFFF";
-                                ctx.fillText(member.user.username, 300, 120);
- 
-                                    ctx.font = '24px Arial Bold';
-                              ctx.fontSize = '15px';
-                              ctx.fillStyle = "#FFFFFF";
-                                ctx.fillText(`${hours}:${minutes}:${seconds} ${suffix}`, 300, 160);
-                                              var guild;
-    while (!guild)
-    var channel = member.guild.channels.find("name","welcome")
-                guild = member.guild
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                        member.guild.fetchInvites().then(invs => {
-      let user = Invite.inviter;
-      let invites = invs.filter(i => i.inviter.id === user.id);
-      let inviteCount = invites.reduce((p, v) => v.uses + p, 0);
-
- channel.send(`**Invited By** : **${Invite.inviter} (${inviteCount})**`);
-
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
 })
-}
-            dat[Inv] = Invite.uses;
-})
-})
-                              ctx.font = '25px Arial';
-                              ctx.fontSize = '28px';
-                              ctx.fillStyle = "#FFFFFF";
-      ctx.fillText(`Welcome To ${member.guild.name}`, 260, 60);
- 
-                              //AVATARً
-                              let Avatar = Canvas.Image;
-                              let ava = new Avatar;
-                              ava.src = buf;
-                              ctx.beginPath();
-                 ctx.arc(149, 118, 98, 0, Math.PI*2, true);
-                   ctx.closePath();
-                   
-                                 ctx.clip();
- 
-                        ctx.drawImage(ava, 49, 17, 200, 200);
-                              ctx.closePath();
- channel.sendFile(canvas.toBuffer())
-                          })
-})
-});
+}});
+     
 client.login(process.env.BOT_TOKEN);
