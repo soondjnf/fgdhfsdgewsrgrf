14bce683d66bba9034083ba969fac5e7ba393f2a
kjhgfdgtyhujhgfvd <40675551+soondjnf@users.noreply.github.com>
2018-09-02 09:46:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 let points = JSON.parse(fs.readFileSync(`./point.json`, `utf8`));
-var shortNumber = require('short-number');
 
 
 let prefix = '*';
